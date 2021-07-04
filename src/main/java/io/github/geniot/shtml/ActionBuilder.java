5a117b79b7be7c3b6f0eec43f1e448bbbcd65ed2
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2021-07-04 16:26:33 +0200

    Using latest source code from https://github.com/freeplane/shtml.

--- src/main/java/io/github/geniot/shtml/ActionBuilder.java
+++
@@ -1,6 +0,0 @@
-package io.github.geniot.shtml;
-
-public interface ActionBuilder {
-
-    void initActions(SHTMLPanel panel);
-}
--- src/main/java/io/github/geniot/shtml/SHTMLPrefsChangeListener.java
+++
@@ -1,5 +0,0 @@
-package io.github.geniot.shtml;
-
-public interface SHTMLPrefsChangeListener {
-	void shtmlPrefChanged(String propertyName, String newValue, String oldValue);
-}
